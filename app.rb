5389c5f961c81fe790a55b7a4f080f4505436b75
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-03-09 16:58:00 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,74 @@
+# file: app.rb
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+
+  post "/albums" do 
+    repo = AlbumRepository.new
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+
+    repo.create(new_album)
+    p repo
+    return ""
+  end 
+  
+  get "/albums" do 
+    repo = AlbumRepository.new
+
+    @albums = repo.all
+    return erb(:albums)
+  end
+
+  get "/artists" do 
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+    
+    artists = []
+    
+    list  = repo.all 
+    list.each do |artist|
+      artists << artist.name
+    end
+    return artists.join(", ")
+  end
+
+  post "/artists" do
+    repo = ArtistRepository.new
+
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+
+    repo.create(new_artist)
+    
+    return ""
+
+  end
+
+  get "/albums/:id" do
+    repo = AlbumRepository.new
+    artist_repo = ArtistRepository.new
+
+
+    @album = repo.find(params[:id])
+    @artist = artist_repo.find(@album.artist_id)
+
+
+    return erb(:album)
+  end
+
+end
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,96 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+
+def reset_artists_table
+  seed_sql = File.read('spec/seeds/artists_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_2' })
+  connection.exec(seed_sql)
+end
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds/albums_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_2' })
+  connection.exec(seed_sql)
+end
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  
+    context "POST /albums" do
+      xit "should create a new album" do 
+
+        response = post('/albums', 
+        title: 'Voyager', 
+        release_year: 2022, 
+        artist_id: 2)
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq("")
+
+        response = get('/albums')
+        expect(response.body).to include('Voyager')
+
+      end 
+    end
+
+    
+    context 'GET /artists' do 
+      xit "returns all artists listed" do
+
+        response = get("/artists")
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq "Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos"
+      end
+    end 
+
+
+    context "POST /artists" do 
+      xit "creates a new artist in the database" do 
+
+        response = post("/artists", 
+        name: "Wild nothing",
+        genre: "Indie"
+        )
+
+        expect(response.status).to eq(200)
+        expect(response.body).to eq("")
+
+        response = get('/artists')
+      
+        expect(response.status).to eq(200)
+        expect(response.body).to include('Wild nothing')
+      end
+    end
+
+    context "GET /albums/:id" do
+    xit "returns the HTML content for a single album 2" do
+      
+      response = get('/albums/2')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<h1>Surfer Rosa</h1>')
+      expect(response.body).to include('Release year: 1988')
+    end 
+  end
+
+  context "GET /albums" do
+    it "returns all albums" do
+      
+      response = get('/albums')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<h1>Albums</h1>')
+      expect(response.body).to include('Title: Surfer Rosa')
+    end 
+  end
+
+end
